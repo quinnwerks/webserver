62baa1142b7269d78b5c05dfb1f56f26cd8d86d5
Quinn Smith <quinn.smith98@gmail.com>
2019-11-24 12:46:05 -0500

    fixed payload interface

--- common/payload.go
+++ common/payload.go
@@ -1,7 +1,16 @@
 package message
 
-import ()
+import (
+    "fmt"
+)
 
+const PING_SIZE = 0
+const GET_SIZE = 1
+const PUT_SIZE = 2
+
+func getSizeError(p Payload) string {
+    return fmt.Sprintf("%T", p)
+}
 
 type Payload interface {
     getContents() []string
@@ -10,14 +19,31 @@ type Payload interface {
 
 type Ping struct {}
 
+func (p *Ping) getContents() []string {
+    contents := make([]string, PING_SIZE)
+    return contents
+}
+
+func (p *Ping) setContents(s []string) {
+    panic(fmt.Sprintf("Can't set contents of a %T", p))
+}
+
+
 type Get  struct {
     Query string
 }
 
-func (g Get) getContents() []string {
-    contents := make([]string, 1)
+func (g *Get) getContents() []string {
+    contents := make([]string, GET_SIZE)
     contents[0] = g.Query
     return contents
+}
+
+func (g *Get) setContents(s []string) {
+    if(len(s) != GET_SIZE) {
+        panic(getSizeError(g))
+    }
+    g.Query = s[0]
 }
 
 type Put  struct {
@@ -25,3 +51,17 @@ type Put  struct {
     Value string
 }
 
+func (p *Put) getContents() []string {
+    contents := make([]string, PUT_SIZE)
+    contents[0] = p.Key
+    contents[1] = p.Value
+    return contents
+}
+
+func (p *Put) setContents(s []string) {
+    if (len(s) != PUT_SIZE) {
+        panic(getSizeError(p))
+    }
+    p.Key = s[0]
+    p.Value = s[1]
+}
