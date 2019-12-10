a96082d27d1ba009ac7cc37bb8eeeca8e932ae7b
Quinn Smith <quinn.smith98@gmail.com>
2019-12-09 21:54:01 -0500

    switched from conn write / read to buffered write / read

--- client/client.go
+++ client/client.go
@@ -1,57 +1,61 @@
 package main
 
 import (
-    "log"
-    "net"
-    "fmt"
-    "time"
-    "github.com/quinnwerks/webserver/message"
+	"bufio"
+	"github.com/quinnwerks/webserver/message"
+	"log"
+	"net"
+	"time"
 )
 
-type Client struct{
-    ServerHost string
-    ServerPort int
-    Socket     net.Conn 
+type Client struct {
+	ServerHost string
+	ServerPort int
+	Socket     net.Conn
+	ReadWriter *bufio.ReadWriter
 }
 
 func (c *Client) SetConnection(host string, port int) {
-    c.ServerHost = host
-    c.ServerPort = port
+	c.ServerHost = host
+	c.ServerPort = port
 }
 
 func (c *Client) Connect() error {
-    conn_type := "tcp"
-    host := c.ServerHost
-    port := c.ServerPort
-    conn_str := host + ":" + string(port)
-    conn, err := net.Dial(conn_type, conn_str)
-    if(err != nil) {
-        log.Printf("%s", err)
-    } else {
-        c.Socket = conn
-    }
+	conn_type := "tcp"
+	host := c.ServerHost
+	port := c.ServerPort
+	conn_str := host + ":" + string(port)
+	conn, err := net.Dial(conn_type, conn_str)
+	if err != nil {
+		log.Printf("%s", err)
+	} else {
+		c.Socket = conn
+	}
 
-    return err
+	return err
 }
 
-func (c * Client) HandleConnectionError(err error) bool{
-    return false
+func (c *Client) HandleConnectionError(err error) bool {
+	return false
 }
 
 func main() {
-    get_msg := message.Message{message.GET, message.Get{Query:"Hello World"}}
-    byt := get_msg.Encode()
-    conn, err := net.Dial("tcp", ":8080")
-    if err != nil {
-        log.Println("Handle dial error")
-    }
-    fmt.Println("Sending: ", string(byt))
-    conn.Write(byt)
-    byt = make([]byte, 512)
-    time.Sleep(time.Second)
-    _, err = conn.Read(byt)
-    fmt.Println(err)
-    fmt.Println("Recieving: ", string(byt))
-    conn.Close()
-    fmt.Println("Here")
+	get_msg := message.Message{Head: message.GET, Body: message.Get{Query: "Hello World"}}
+	byt := get_msg.Encode()
+	conn, err := net.Dial("tcp", ":8080")
+	if err != nil {
+		log.Println("Handle dial error")
+	}
+	log.Printf("Sending: %s", string(byt))
+
+	conn_handler := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+	_, err = conn_handler.Writer.WriteString(string(byt) + "\n")
+	log.Printf("Error after writer: %s", err)
+	err = conn_handler.Writer.Flush()
+	log.Printf("Error after flush: %s", err)
+	time.Sleep(time.Second)
+	byt, err = conn_handler.Reader.ReadBytes('\n')
+	log.Printf("Error after read %s", err)
+	log.Printf("Recieving: %s", string(byt))
+	conn.Close()
 }
--- server/server.go
+++ server/server.go
@@ -1,30 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "bufio"
+	"bufio"
+	"log"
+	"net"
 )
 
-
 func main() {
-    ln, err := net.Listen("tcp", ":8080")
-    if err != nil {
-    }
+	log.Printf("Server initalized")
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+	}
 
-    for {
-        conn, err := ln.Accept()
-        if err != nil {
-            // handle error
-        }
-        go handleConnection(conn)
-    }
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		go handleConnection(conn)
+	}
 }
 
 func handleConnection(conn net.Conn) {
-    msg, thing := bufio.NewReader(conn).ReadString('\n')
-    fmt.Println(conn, msg, thing)
+	conn_handler := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+	byt, err := conn_handler.Reader.ReadBytes('\n')
+	log.Printf("Error on read: %s", err)
+	log.Printf("Recieving: %s", string(byt))
+	_, err = conn_handler.Writer.WriteString(string(byt) + "\n")
+	log.Printf("Error after writer: %s", err)
+	err = conn_handler.Writer.Flush()
+	log.Printf("Error after flush: %s", err)
 }
-
-
-
